362b991446462ebd84acc1aa512acd28431ba828
cynic <kurisufag1@gmail.com>
2025-02-15 11:19:48 -0500

    final touches to the deploy stack + all working nodes

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, sys
 
 manifest = []
 
@@ -14,6 +14,7 @@ pkgs = [
     "curl",
     "beep"
 ]
+manifest.append("apt update")
 manifest.append("apt install -y " + " ".join(pkgs))
 
 
@@ -21,8 +22,8 @@ manifest.append("apt install -y " + " ".join(pkgs))
 
 manifest.append("mkdir -p /mnt/chip")
 # idempotent as heck
-manifest.append("(echo \\\"@reboot sshfs /mnt/chip/ chip@alexandria.wuvt.vt.edu:/tank/archive/rips/chip -o IdentityFile=/root/.ssh/$(hostname)-whoreslayer-node\\\") | crontab -")
-manifest.append("(crontab -l; echo \\\"*/1 * * * * [ -d /mnt/chip/tools ] || sshfs /mnt/chip/ chip@alexandria.wuvt.vt.edu:/tank/archive/rips/chip -o IdentityFile=/root/.ssh/$(hostname)-whoreslayer-node\\\") | crontab -")
+manifest.append("(echo \\\"@reboot sshfs /mnt/chip/ chip@alexandria.wuvt.vt.edu:/tank/archive/rips/chip -o IdentityFile=/root/.ssh/$$$HOSTNAME$$$-whoreslayer-node\\\") | crontab -")
+manifest.append("(crontab -l; echo \\\"*/1 * * * * [ -d /mnt/chip/tools ] || sshfs /mnt/chip/ chip@alexandria.wuvt.vt.edu:/tank/archive/rips/chip -o IdentityFile=/root/.ssh/$$$HOSTNAME$$$-whoreslayer-node\\\") | crontab -")
 
 # install systemd service (re-installs every time: idempotent by default)
 link = "https://raw.githubusercontent.com/kurisufriend/chip/refs/heads/master/tools/service/chip-client.service"
@@ -36,6 +37,8 @@ manifest.append("systemctl start chip-client.service")
 with open("nodes.json", "r") as f:
     nodes = json.loads(f.read())
     for n in nodes:
+        if "--specify" in sys.argv and not n["host"] in sys.argv:
+            continue
         for cmd in manifest:
             print(">> " + cmd)
-            os.system(f"ssh root@{n['ipv4']} \"{cmd}\"")
+            os.system(f"ssh root@{n['ipv4']} \"{cmd}\"".replace("$$$HOSTNAME$$$", n["host"]))
